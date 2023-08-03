0c516384d5c3c5b3c67b30a27ce486409d8e6bfc
Magui Brollo <maguieb@hotmail.com>
2023-08-02 23:45:08 -0300

    Finish

--- scriptVerMas.js
+++ scriptVerMas.js
@@ -3,20 +3,20 @@ var url_string = window.location.href
 var url = new URL(url_string);
 var id_un_personaje = parseInt(url.searchParams.get("id"));
  
-let eepi=[];
-
+var mas_episode='';
 //---------------------------- UNO ----------------------------
-const uno_nombre = document.getElementById('nombre-uno');
-const uno_personaje_vermas = document.getElementById('un_personaje');
+var uno_nombre = document.getElementById('nombre-uno');
+var uno_personaje_vermas = document.getElementById('un_personaje');
+var uno_cont_episodios = document.getElementById('cont-episodios');
 
-const uri = `https://rickandmortyapi.com/api/character/${id_un_personaje}`;
+let uri = `https://rickandmortyapi.com/api/character/${id_un_personaje}`;
 
 let respuestaFetch = fetch(uri);
 respuestaFetch.then((respuesta) => {
     return respuesta.json();
 })
     .then((data) => {
-        eepi=data.episode;
+        
         uno_nombre.innerHTML += `<h2> Personaje seleccionado: ${data.name} </h2>`
         uno_personaje_vermas.innerHTML = `
             <div class="card-personaje-uno">
@@ -27,61 +27,66 @@ respuestaFetch.then((respuesta) => {
         
                 <div class="info-card-personaje-uno">
                     <p>
-                        Nombre   : ${data.name}<br>
+                        <b>Nombre   : ${data.name} </b><br>
                         Género   : ${data.gender}  <br>
                         Especie  : ${data.species} <br>
                         Estatus  : ${data.status} <br>
                         Creado   : ${data.created} <br>
                         <br>
-                        Origen: <br>
+
+                        <b>Origen:</b> <br>
                         -Nombre: ${data.origin.name} <br>
                         -URL   : <a href="${data.origin.url}"> URL origen</a> <br>
                         
                         <br>
-                        Ubicación: <br>
+                        <b>Ubicación: </b><br>
                         -Nombre: ${data.location.name} <br>
                         -URL   : <a href="${data.location.url}"> URL Ubicación</a> <br>
                         <br>
-                        Episodios:  <br>
-                        -URL   : <a href="${data.episode[1]}"> Episodio 1</a> <br>
-                        <br><br>
+                        <b>Episodios:</b> ${data.episode.length} episodios (ver a continuación:) 
+                       
                     </p>
                 </div>
-        
             </div>
-            `
+            ` 
+            mostrar_episodios(data.episode); 
+
     }).catch((error) => {           //atrapa error
         console.log(error);
     });
 
-
-/* //----------------------
-function buscar_episodio(uri){
-    fetch(uri)
-    .then((data) => {
-        console.log(datos);
-        return datos.json();
-    })
-        .then((data) => {
-            console.log(data);
-            return data;
-        })
+//----------------------
+function buscar_episodios(x){
+    fetch(x)
+    .then((episode)=>{
+        return episode.json();
+    }).then((epi_data)=>{
+        //console.log(mas_episode)
+        uno_cont_episodios.innerHTML +=`
+                <div class="epi">
+                    <p>
+                    <b> Nombre .......: ${epi_data.name} </b><br>
+                        Fecha Estreno.: ${epi_data.air_date} <br>
+                        Espisodio.....: ${epi_data.episode} <br>
+                        url...........: ${epi_data.url} <br>
+                        Fecha Creado..: ${epi_data.created} <br>
+                    </p>
+                </div>` 
+    });    
 }
 
 
-    eepi.forEach(epi => {
-        let ep = buscar_episodio(epi);
-        console.log(ep);
-        uno_personaje_vermas.innerHTML +=`
-        <p>
-            Nombre .......: ${ep.name}<br>
-            Fecha Estreno.: ${ep.air_date}  <br>
-            Espisodio.....: ${ep.episode} <br>
-            url...........: ${ep.url} <br>
-            Fecha Creado..: ${ep.created} <br>
-        </p> `  
-    });    */
+function mostrar_episodios(x){
+    console.log(x.length);
     
+   for (let i=0; i< x.length; i++){
+        
+        buscar_episodios(x[i]);
+                         
+    };
+    
+    //uno_personaje_vermas.innerHTML += mas_episode;
+}
 //-------------------- el siguiente CODIGO es para cuando pasan mas d eun parametro
 
 /**
